b90276a499d6c24eb61603f2cff750390ecb56b3
Wang-QP <649537825@qq.com>
2023-12-30 15:23:41 +0800

    1.0

--- new.bat
+++ new.bat
@@ -3,4 +3,4 @@ set /p name=input pages name:
 
 echo name:%name%
 
-hexo n "%name%" && start /d "D:\Typora"  Typora.exe  "E:\code\hexo\source\_posts\%name%.md"
+hexo n "%name%" && start /d "D:\App\Typora"  Typora.exe  "D:\Data\hexo\source\_posts\%name%.md"
---
+++ source/_posts/01-jvm内存区域.md
@@ -0,0 +1,6 @@
+---
+title: 01-jvm内存区域
+date: 2023-12-29 21:53:40
+categories: java
+tags: jvm
+---
